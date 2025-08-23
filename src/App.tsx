1e0f1f72b7ef6eac0b4b5b77979088449b3900f9
surafel <surafelmengist2015@gmail.com>
2025-08-23 18:18:30 +0300

    types

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
-// normal array in ts
-// In TypeScript, an array is a collection of values of the same type (or sometimes a union of types).
-const App = () => {
- 
-  // Method 1: Using []
-  const numbers: number[] = [1, 2, 3, 4];
+type AppProps = {
+  name: string;
+  age: number;
+  isAdmin: boolean;
+};
 
-// Method 2: Using Array<type>
-const fruits: Array<string> = ["apple", "banana", "mango"];
+const user: AppProps = {
+  name: "surafel",
+  age: 23,
+  isAdmin: false, // ✅ matches type
+};
+
+const App = ({ name, age, isAdmin }: AppProps) => {
   return (
     <div>
-          {/* two excute array in the dom we can use map(),foreach or other methods */}
-          {fruits.map((fr)=>{
-            return <p>{fr}</p>
-          })}
-          {numbers.map((num)=>{
-            return <p>{num}</p>
-          })}
+      <p>My name is {name}</p>
+      <p>I am {age} years old</p>
+      <p>{isAdmin ? "You are admin" : "You are not admin"}</p>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default function Root() {
+  return <App {...user} />; // ✅ passing props correctly
+}
