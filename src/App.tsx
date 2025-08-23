83db8cfb1154f48d7fe6f55e76bd77d2cf59ccae
surafel <surafelmengist2015@gmail.com>
2025-08-23 11:54:00 +0300

    basic types in ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 
 
-const App = () => {
+ const App = () => {
+  const myName: string = "Surafel";   // text
+   const age: number = 25;             // numbers (int, float, etc.)
+   const isStudent: boolean = true;    // true / false
+   const nothing: null = null;         // null
+   const notAssigned: undefined = undefined; // undefined
+   const big: bigint = 100n;           // big integer
+   const uniqueId: symbol = Symbol("id"); // unique value
+ 
   return (
     <div>
-           <h1>The first one</h1>
+         <p>my name is {myName}</p>
+         <p>i am {age} years old</p>
+         <p>{isStudent ? "you are a student":"you are not a student"}</p>
+         <p>{nothing ? "nothing": "some output"}</p>
+         <p>{notAssigned ? "undefind": "defind"}</p>
+         <p>{big}</p>
+         <p>{uniqueId ? "unigue id": "invalid input"}</p>
     </div>
   )
 }
