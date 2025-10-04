d9c8386e4e2d28587e9d522f653e410eef69ef8d
surafel <surafelmengist2015@gmail.com>
2025-10-04 15:23:14 +0300

    usesearch params

--- src/Components/StudentInfo.tsx
+++ src/Components/StudentInfo.tsx
@@ -1,11 +1,19 @@
 import { useParams } from "react-router-dom"
+import { useSearchParams } from "react-router-dom"
+
 const StudentInfo = () => {
 const params=  useParams<{ id: string }>()
 const studentId=params.id
+const [searchParams, setSearchParams] = useSearchParams()
+const active=searchParams.get('active')
+const reset=searchParams.get('reset')
+console.log(active,reset)
   return (
     <div>
             <h1> student list page</h1>
             <p>Student ID: {studentId}</p>
+            <button className="bg-pink-700 text-white p-4  rounded m-2 text-center" onClick={() => setSearchParams({ active: 'true' })}>Active user</button>
+            <button className="bg-pink-700 text-white p-4  rounded m-2 text-center" onClick={() => setSearchParams({ reset: 'true' })}>reset user</button>
     </div>
   )
 }
